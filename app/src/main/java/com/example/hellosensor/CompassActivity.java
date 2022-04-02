8ea1ec4414485e0caa9289e892acababe381e726
Isak <isakmarklund@gmail.com>
2022-04-02 15:09:22 +0200

    kompassen fick en ikon + lågpassfilter på sensordatan i kompassen

--- app/src/main/java/com/example/hellosensor/CompassActivity.java
+++ app/src/main/java/com/example/hellosensor/CompassActivity.java
@@ -7,11 +7,15 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 public class CompassActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView dTV;
+    private ImageView cIV;
     private SensorManager mSensorManager;
     private Sensor mMagnetic;
     private Sensor mAccelerometer;
@@ -19,6 +23,10 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     private float[] magReadings = new float[3];
     private float[] rotationMatrix = new float[9];
     private float[] orientationAngles = new float[3];
+    private float startDegrees = 0f;
+    private float currentDegrees = 0f;
+    private double[] recentReadings = new double[5];
+    private static final float ALPHA = 0.1f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,7 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         mMagnetic = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         dTV = (TextView) findViewById(R.id.tvDegrees);
+        cIV = (ImageView) findViewById(R.id.ivCompass);
     }
     @Override
     protected void onPause() {
@@ -51,17 +60,19 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
             return;
         }
         if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            for(int i = 0; i < 3; i++){
-                accReadings[i] = event.values[i];
-            }
+            //for(int i = 0; i < 3; i++){
+            //    accReadings[i] = event.values[i];
+            //}
+            accReadings = applyLowPassFilter(event.values, accReadings);
         } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
-            for(int i = 0; i < 3; i++){
-                magReadings[i] = event.values[i];
-            }
+            //for(int i = 0; i < 3; i++){
+            //    magReadings[i] = event.values[i];
+            //}
+            magReadings = applyLowPassFilter(event.values, magReadings);
         }
         updateOrientationAngles();
-
     }
+
 
     private String getDirection(double angle){
         StringBuilder direction = new StringBuilder();
@@ -100,7 +111,41 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         double angle = Math.round(degrees * 100) / 100;
         String direction = getDirection(degrees);
         dTV.setText(angle + " degrees " + direction);
+        cIV.setRotation((float) angle * -1);
     }
+
+    /**
+     * Moving average filter - jobbar med grader, men borde jobba med radianer eller något så att den inte ballar ur vid när den korsar 0/360 grader strecket
+     * **/
+    private double maFilter(double degrees){
+        double sin = 0;
+        double cos = 0;
+        for(int i = recentReadings.length - 1; i > 0; i--){
+            // update recent recentReadings
+            recentReadings[i] = recentReadings[i-1];
+        }
+        recentReadings[0] = degrees;
+        for(double ang : recentReadings){
+            //sum += ang;
+            sin += Math.sin(ang);
+            cos += Math.cos(ang);
+        }
+        double avSin = sin / recentReadings.length;
+        double avCos = cos / recentReadings.length;
+        double average = Math.atan(sin/cos);
+        return average;
+    }
+
+    private float[] applyLowPassFilter(float[] input, float[] output) {
+        if ( output == null ) return input;
+
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + ALPHA * (input[i] - output[i]);
+        }
+        return output;
+    }
+
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
